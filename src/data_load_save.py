de500a62eeef967f57cb1c47c45cc6d744fc1423
Park Jimin <parkjimin788@gmail.com>
2025-11-30 21:36:36 +0900

    ㅇㅇ

--- src/data_load_save.py
+++ src/data_load_save.py
@@ -163,28 +163,6 @@ def sync_parquet_and_postgres(parquet_folder: str, postgres_uri: str):
 
 
 
-def basic_path():
-    print(
-        """
-doc_path = "/home/user/문서"
-
-down_path = f"/home/user/다운로드"
-
-
-
-parquet_path = "/home/user/GoogleDrive/data"
-csv_path = f"{doc_path}/csv_data"
-
-focus = "agrifood_elasticity_usa"  # 필요시 다른 키로 변경
-
-raw_csv_file = f"{down_path}/raw_data/aaa.csv"
-
-csv_file = f"{csv_path}/{focus}.csv"
-parquet_file = f"{parquet_path}/{focus}.parquet"
-
-postgres_uri = "postgresql+psycopg2://supersetuser:StrongPassword123!@localhost:5432/parquetsyncdb"
-        """
-    )
 
 
 #############################################################################
--- yaml/path.py
+++ yaml/path.py
@@ -1,6 +1,17 @@
+doc_path = "/home/user/문서"
+
+down_path = f"/home/user/다운로드"
+
+
+
+parquet_path = "/home/user/GoogleDrive/data"
+csv_path = f"{doc_path}/csv_data"
+
 focus = "agrifood_elasticity_usa"  # 필요시 다른 키로 변경
 
-raw_csv_file = f"/home/user/다운로드/raw_data/aaa.csv"
+raw_csv_file = f"{down_path}/raw_data/aaa.csv"
 
-csv_file = f"/home/user/문서/csv_data/{focus}.csv"
-parquet_file = f"/home/user/GoogleDrive/data/{focus}.parquet"
+csv_file = f"{csv_path}/{focus}.csv"
+parquet_file = f"{parquet_path}/{focus}.parquet"
+
+postgres_uri = "postgresql+psycopg2://supersetuser:StrongPassword123!@localhost:5432/parquetsyncdb"
